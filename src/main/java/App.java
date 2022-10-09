840e61fee3279a3e187f59793e648fd8a9193d65
AnatolyEfremov <110206057+AnatolyEfremov@users.noreply.github.com>
2022-10-09 19:07:41 +0300

    Add files via upload

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld anotherBean = applicationContext.getBean(HelloWorld.class);
+        System.out.println(bean == anotherBean);
+
+        Cat cat1 = applicationContext.getBean(Cat.class);
+        Cat cat2 = applicationContext.getBean(Cat.class);
+        System.out.println(cat1 == cat2);
+
+    }
+}
